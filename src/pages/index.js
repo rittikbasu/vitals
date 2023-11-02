26d0bf4f9f14f5fdfa29b0f10a5bfb6a6c4dfbd6
rittikbasu <irittik@gmail.com>
2023-11-02 07:27:38 +0530

    feat: add meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Head from "next/head";
 import { createClient } from "@supabase/supabase-js";
 import {
   Table,
@@ -15,61 +16,70 @@ export default function Home({ vitals }) {
   const [tableData, setTableData] = useState(vitals);
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pb-14">
-      <h1 className="text-2xl font-mono text-green-200 text-center py-8">
-        Vitals
-      </h1>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell className="text-center dark:text-blue-300">
-              Date
-            </TableHeaderCell>
-            <TableHeaderCell className="text-center dark:text-blue-300">
-              Time
-            </TableHeaderCell>
-            <TableHeaderCell className="text-center dark:text-blue-300">
-              <span className="hidden lg:block">Blood Pressure</span>
-              <span className="lg:hidden block">BP</span>
-            </TableHeaderCell>
-            <TableHeaderCell className="text-center dark:text-blue-300">
-              Pulse
-            </TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableData.map((entry, index) => (
-            <TableRow key={index}>
-              <TableCell
-                className="text-center dark:text-zinc-400/80"
-                // onClick={() => console.log(entry.date)}
-              >
-                {entry.date}
-              </TableCell>
-              <TableCell className="text-center dark:text-zinc-400/80">
-                {entry.time}
-              </TableCell>
-              <TableCell className="text-center dark:text-zinc-400/80">
-                {entry.bp}
-              </TableCell>
-              <TableCell className="text-center dark:text-zinc-400/80">
-                {entry.pulse}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-center items-center">
-        <Button onClick={() => setIsModalOpen(true)}>Add</Button>
-      </div>
-      {isModalOpen && (
-        <Modal
-          onClose={() => setIsModalOpen(false)}
-          tableData={tableData}
-          setTableData={setTableData}
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-      )}
-    </div>
+        <title>Vitals</title>
+      </Head>
+      <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 pb-14 bg-zinc-900/70">
+        <h1 className="text-2xl font-mono text-green-200 text-center py-8">
+          Vitals
+        </h1>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell className="text-center dark:text-blue-300">
+                Date
+              </TableHeaderCell>
+              <TableHeaderCell className="text-center dark:text-blue-300">
+                Time
+              </TableHeaderCell>
+              <TableHeaderCell className="text-center dark:text-blue-300">
+                <span className="hidden lg:block">Blood Pressure</span>
+                <span className="lg:hidden block">BP</span>
+              </TableHeaderCell>
+              <TableHeaderCell className="text-center dark:text-blue-300">
+                Pulse
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData.map((entry, index) => (
+              <TableRow key={index}>
+                <TableCell
+                  className="text-center dark:text-zinc-400/80"
+                  // onClick={() => console.log(entry.date)}
+                >
+                  {entry.date}
+                </TableCell>
+                <TableCell className="text-center dark:text-zinc-400/80">
+                  {entry.time}
+                </TableCell>
+                <TableCell className="text-center dark:text-zinc-400/80">
+                  {entry.bp}
+                </TableCell>
+                <TableCell className="text-center dark:text-zinc-400/80">
+                  {entry.pulse}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-center items-center">
+          <Button onClick={() => setIsModalOpen(true)}>Add</Button>
+        </div>
+        {isModalOpen && (
+          <Modal
+            onClose={() => setIsModalOpen(false)}
+            tableData={tableData}
+            setTableData={setTableData}
+          />
+        )}
+      </div>
+    </>
   );
 }
 const convertTo24Hour = (time) => {
